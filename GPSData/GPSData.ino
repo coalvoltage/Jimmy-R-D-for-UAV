4d152abd46f2b2b234ae121bd17d910b5f33365d
Jimmy Im <jim011@ucr.edu>
2019-05-11 13:53:30 -0700

    dumbstuff

--- GPSData/GPSData.ino
+++ GPSData/GPSData.ino
@@ -5,15 +5,15 @@ SoftwareSerial gpsSerial(3,4);//rx,tx
 TinyGPS gps; // create gps object 
 
 void setup(){ 
-  Serial.begin(9600); // connect serial 
-  //Serial.println("The GPS Received Signal:"); 
-  gpsSerial.begin(9600); // connect gps sensor
+  Serial.begin(9600); // be sure to check lower right corner info in an open 'serial windows'
+  gpsSerial.begin(9600);  // may be 4800, 19200,38400 or 57600
 } 
 void loop(){ 
-      while(gpsSerial.available()){ // check for gps data 
-        if(gps.encode(gpsSerial.read()))// encode gps data 
+  while(gpsSerial.available()){ // check for gps data 
+    
+    if(gps.encode(gpsSerial.read()))// encode gps data 
+      {
         Serial.println("awoo");
-      {  
         gps.f_get_position(&lat,&lon); // get latitude and longitude 
         // display position 
         Serial.print("Position: "); 
@@ -25,9 +25,24 @@ void loop(){
         Serial.print(lat); 
         Serial.print(" "); 
       } 
-    } 
+  } 
   String latitude = String(lat,6); 
   String longitude = String(lon,6); 
   Serial.println(latitude+";"+longitude); 
   delay(1000); 
-} 
+}
+/*
+#include <SoftwareSerial.h>
+
+SoftwareSerial gps(3,4); // RX, TX  Connect GPS TX to pin 3
+void setup()
+{
+  Serial.begin(115200); // be sure to check lower right corner info in an open 'serial windows'
+  gps.begin(9600);  // may be 4800, 19200,38400 or 57600
+
+}
+
+void loop()
+{
+  if (gps.available())  Serial.write(gps.read());
+}*/
